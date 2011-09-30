a1c598838294a5dc26706ea3ce00d5c5128b8d87
Faizan <faizan@faizan-ubuntu.(none)>
2011-09-30 18:13:49 +0530

    For mobile app: used signpost library for android oauth,request_token recieved

--- Dropbox/src/net/practice/dropbox/DropboxActivity.java
+++ Dropbox/src/net/practice/dropbox/DropboxActivity.java
@@ -1,7 +1,13 @@
 package net.practice.dropbox;
 
-import org.apache.http.HttpResponse;
-
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.basic.DefaultOAuthConsumer;
+import oauth.signpost.basic.DefaultOAuthProvider;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import oauth.signpost.exception.OAuthNotAuthorizedException;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -10,21 +16,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
-//import java.util.Map;
-import net.oauth.consumer.OAuth2Consumer;
-import net.oauth.enums.GrantType;
-import net.oauth.enums.ResponseType;
-import net.oauth.exception.OAuthException;
-import net.oauth.parameters.OAuth2Parameters;
-import net.oauth.provider.OAuth2ServiceProvider;
 
-@SuppressWarnings("unused")
 public class DropboxActivity extends Activity {
 	private final static String APP_KEY = "iypm3vb6d6vxha0";
     private final static String APP_SECRET = "e1kq1rhpwu6dfhx";
+    
+    
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         /** Create the Button and Text View
@@ -39,37 +41,67 @@ public class DropboxActivity extends Activity {
         	public void onClick(View v) {
 			// TODO Auto-generated method stub
 			text.setText(mainText.getText().toString()+"1");
+			
 			try {
-			OAuth2ServiceProvider provider = new OAuth2ServiceProvider("http://api.dropbox.com/0/oauth/request_token", "https://api.dropbox.com/0/token");
-			try{
-            OAuth2Consumer consumer = new OAuth2Consumer(APP_KEY, APP_SECRET, provider);
-			}catch(Exception e)
-			{
-				text.setText(e.getMessage());
-			}
-            
-            
-              //  text.setText(consumer.generateRequestAuthorizationUrl(ResponseType.CODE, "http://localhost:8080/my_app/oauth_redirect", null, (String[])null).toString());
-                
-                //OAuth2Parameters parameters = new OAuth2Parameters();
-                //parameters.setCode("3f61aa47b915215a938d2722-682316653|5OPOkmKew_W8vybb9sccIPoivAg.");
-                //parameters.setRedirectUri("http://localhost:8080/my_app/oauth_redirect");
-                
-                //HttpResponse oauthResponse = (HttpResponse) consumer.requestAcessToken(GrantType.AUTHORIZATION_CODE, parameters, (String[])null);
-                
-                //text.setText(oauthResponse.toString());
-        } catch (Exception e) {
-                // TODO Auto-generated catch block
-                mainText.setText(e.toString());
-        }
+				OAuthProvider provider = new DefaultOAuthProvider
+	            ("https://api.dropbox.com/0/oauth/request_token",
+	             "https://api.dropbox.com/0/oauth/access_token",
+	             "https://www.dropbox.com/0/oauth/authorize");
+				//provider.setOAuth10a(true);
+	        OAuthConsumer consumer = new DefaultOAuthConsumer
+	            (APP_KEY, APP_SECRET);
 
-            
-            
+	        /*
+	         * Brings Acess Token from Dropbox
+	         * String url = provider.retrieveRequestToken(consumer, "MobConnect://dropbox");
+	        mainText.setText(url);
+	        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+	        startActivity(browserIntent);*/
+	        
+	        //Now we use that token which user enters into text box for getting access_token
+	        String oauthToken= mainText.getText().toString();
+	        provider.retrieveAccessToken(consumer, oauthToken);
+	        mainText.setText("token="+consumer.getToken()+" token secret="+
+                           consumer.getTokenSecret());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				mainText.setText(mainText.getText().toString()+e.getLocalizedMessage());
+			}				
+			
 		}
       
         });
-
+    
     }
-    
-    
+     
+   
+    /*@Override
+    public void onResume()
+    {
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        String oauthToken = uri.getQueryParameter("oauth_token");
+        final TextView text = (TextView) findViewById(R.id.textView1);        
+     
+        OAuthProvider provider = new DefaultOAuthProvider
+        ("https://api.dropbox.com/0/oauth/request_token",
+         "https://api.dropbox.com/0/oauth/access_token",
+         "https://www.dropbox.com/0/oauth/authorize");
+        OAuthConsumer consumer = new DefaultOAuthConsumer
+        (APP_KEY, APP_SECRET);
+try{
+        provider.retrieveAccessToken(consumer, oauthToken);
+}catch(OAuthCommunicationException e){
+	text.setText(e.toString());
+} catch (OAuthMessageSignerException e) {
+	// TODO Auto-generated catch block
+	text.setText(e.toString());
+} catch (OAuthNotAuthorizedException e) {
+	// TODO Auto-generated catch block
+	text.setText(e.toString());
+} catch (OAuthExpectationFailedException e) {
+	// TODO Auto-generated catch block
+	text.setText(e.toString());
+}
+    }*/
 }
