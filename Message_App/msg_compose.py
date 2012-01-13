7b5338a6f2c3066ce6569d6d09f70cd7c8435f93
Bhagyashree Mandora <bhagyashree.mandora@gmail.com>
2012-01-13 14:14:27 +0530

    Desktop Message App: Listening daemon listens to file on Dropbox as a service. Messages can be queued in dropbox file as a list of JSON objects. Some GUI changes. More need to be done.

---
+++ Message_App/msg_compose.py
@@ -0,0 +1,141 @@
+import pygtk
+pygtk.require('2.0')
+import gtk
+import json_crafting
+
+
+class msgcompose:
+
+    # On clicking the send button, takes text from buffer
+    def sending_msg(self, widget, data):
+        self.pbar.show()
+        startiter, enditer = self.textbuffer1.get_bounds()
+        msg = self.textbuffer1.get_text(startiter, enditer, include_hidden_chars=True)
+        print msg
+
+        message = {"to_number": "9158077350", "from_number": "9764344549", "type": "text", "priority": "normal", "message": msg}
+        
+        craft = json_crafting.Crafting()
+        craft.SEND(message)
+        print message
+        #self.pbar.hide()
+
+
+    def save_msg(self, widget, data):
+        var = raw_input("Enter file name: ")
+        fil = open('Drafts/' + var,'w')
+        fil.writelines('To number: ' + ' ')
+        startiter, enditer = self.textbuffer1.get_bounds()
+        msg = self.textbuffer1.get_text(startiter, enditer, include_hidden_chars=True)
+        fil.writelines('\nContents: ' + msg)
+        fil.writelines('\nDate: ')
+        fil.writelines('\nTime: ')
+        fil.close()
+        
+        raw_input('Message Saved!')
+        gtk.main_quit()
+        return False
+        
+
+    # Causing the close button to close the window
+    def delete_event(self, widget, event, data=None):
+        #self.textbuffer1.delete(0)
+        gtk.main_quit()
+        return False
+
+
+    # The GUI Description of the Window
+    def __init__(self):
+    
+        # Icon trial
+        self.info =  gtk.StatusIcon()
+        self.info.set_from_icon_name("Our App")
+        #self.info.set_from_gicon("msg.jpg")
+
+        # Create a new window
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("Compose Message")
+        self.window.connect("delete_event", self.delete_event)
+        self.window.set_border_width(10)
+        self.window.set_size_request(300,200)
+        self.window.set_position(gtk.WIN_POS_CENTER)
+        
+        # Vertical box to pack widgets
+        self.box1 = gtk.VBox(False, 5)
+        self.window.add(self.box1)
+
+
+        # Horizontal Box in the vertical box
+        self.box2 = gtk.HBox(True, 0)
+        self.box1.pack_start(self.box2, True, True, 0)
+      
+        # A ComboBox to get 'Send To' option
+        self.sendto_combo = gtk.ComboBoxEntry()
+        #self.sendto_combo.set_use_arrows_always(True)
+        self.box1.pack_start(self.sendto_combo, True, True, 0)
+
+      # fr = open('contacts','r')
+      # i= 1
+      # while i <> EOF
+      # contact = fr.readline()
+      # print contact
+      # self.sendto_combo.set_entry_text_column(contact)    
+      # fr.close()
+
+        #Text Box
+        self.textedit = gtk.TextView()
+        self.textbuffer1 = self.textedit.get_buffer()
+        self.scrolled_window = gtk.ScrolledWindow()
+        self.scrolled_window.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.box1.pack_start(self.scrolled_window,True,True,5)
+        self.scrolled_window.add(self.textedit)
+
+
+        # Horizontal Box in the vertical box
+        self.box3 = gtk.HBox(True, 5)
+        self.box1.pack_start(self.box3, True, True, 0)
+
+
+        # Discard button to get read and discard message.
+        self.button4 = gtk.Button("Discard")
+        self.button4.connect("clicked", self.delete_event, "button 4")
+        self.box3.pack_start(self.button4, True, True, 0)
+
+        # Send button to send message i.e. write to file.
+        self.button2 = gtk.Button("Send")
+        self.button2.connect("clicked", self.sending_msg, "button 2")
+        self.box3.pack_start(self.button2, True, True, 0)
+
+        # Save button to save the message in a file.
+        self.button3 = gtk.Button("Save")
+        self.button3.connect("clicked", self.save_msg, "button 3")
+        self.box3.pack_start(self.button3, True, True, 0)
+
+        # Progress bar to show sending msg
+        self.pbar= gtk.ProgressBar()
+        self.pbar.set_text("Sending Message...")
+        self.box1.pack_start(self.pbar, True, True, 0)
+        
+
+        # Show Widgets
+        self.sendto_combo.show()
+        self.textedit.show()
+        self.scrolled_window.show()
+        self.button2.show()
+        self.button3.show()
+        self.button4.show()
+        #self.pbar.show()
+        self.box1.show()
+        self.box2.show()
+        self.box3.show()
+        self.window.show()
+        
+                               
+
+def main():
+    gtk.main()
+
+
+if __name__ == "__main__":
+    cmpo = msgcompose()
+    main()
